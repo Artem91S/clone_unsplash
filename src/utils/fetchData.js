99b82a66857904d30bb3756e8549bb502816dd40
Артём Ситников <sitnikov.artem91@gmail.com>
2023-12-10 22:43:36 +0200

    add env file

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,6 @@
 export async function fetchData(props) {
     const resp = await fetch(
-      `https://api.unsplash.com/photos?client_id=${import.meta.env.VITE_API_KEY}&per_page=11&page=${props.pageParam}`
+      `https://api.unsplash.com/photos?client_id=Djh__xzu7c6vZ1HbNEgeye1iR65ud8gOTeVMB2tkS8c&per_page=11&page=${props.pageParam}`
     );
     return resp.json();
   }
