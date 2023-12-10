39b0fe80a542fd1653707c3000c61dfeedb6b140
Артём Ситников <sitnikov.artem91@gmail.com>
2023-12-10 22:05:02 +0200

    add env file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Layout from "./router";
 
+
 function App() {
   return (
     <>
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,6 @@
 export async function fetchData(props) {
     const resp = await fetch(
-      `https://api.unsplash.com/photos?client_id=Djh__xzu7c6vZ1HbNEgeye1iR65ud8gOTeVMB2tkS8c&per_page=11&page=${props.pageParam}`
+      `https://api.unsplash.com/photos?client_id=${import.meta.env.VITE_API_KEY}&per_page=11&page=${props.pageParam}`
     );
     return resp.json();
   }
@@ -8,7 +8,7 @@ export async function fetchData(props) {
 
   export async function fetchDataLink(props) {
     const resp = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_API_KEY}=${props.queryKey[0]}&per_page=30&page=${props.pageParam}`
+      `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}&query=${props.queryKey[0]}&per_page=30&page=${props.pageParam}`
     );
     return resp.json();
   }
@@ -16,19 +16,19 @@ export async function fetchData(props) {
  export async function fetchPhoto(photoId) {
     // console.log(props);
     const resp = await fetch(
-      `https://api.unsplash.com/photos/${photoId}?client_id=${process.env.REACT_APP_API_KEY}`
+      `https://api.unsplash.com/photos/${photoId}?client_id=${import.meta.env.VITE_API_KEY}`
     );
     return resp.json();
   }
   export async function fetchSearchPhoto(props) {
     const resp = await fetch(
-      `https://api.unsplash.com/search/photos?client_id=${process.env.REACT_APP_API_KEY}=${props.queryKey[0]}&per_page=30&page=${props.pageParam}`
+      `https://api.unsplash.com/search/photos?client_id=${import.meta.env.VITE_API_KEY}&query=${props.queryKey[0]}&per_page=30&page=${props.pageParam}`
       );
     return resp.json();
   }
   export async function fetchSearchCategoria(props) {
     const resp = await fetch(
-      `https://api.unsplash.com/search/collections?client_id=${process.env.REACT_APP_API_KEY}=${props.queryKey[0]}&per_page=30&page=${props.pageParam}`
+      `https://api.unsplash.com/search/collections?client_id=${import.meta.VITE_API_KEY}&query=${props.queryKey[0]}&per_page=30&page=${props.pageParam}`
       );
     return resp.json();
   }
